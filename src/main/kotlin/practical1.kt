3e2a284b8eee10b2b23c1c1f0b05ec81a4271198
dhavanikgithub <kanzariyagovindbhai21@gnu.ac.in>
2022-08-06 09:46:23 +0530

    Printed Data Types

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,3 +1,11 @@
 fun main(){
     println("Hello World")
+    var a : Int = 5
+    val b : Int = 6
+    println("a = $a")
+    println("b = $b")
+    var c = 50.5f
+    var d : Double = 5.45
+    println("c = $c")
+    println("d = $d")
 }
