22ce5b05300eae55c96db5bc4eb5f762c2e2caa8
dhavanikgithub <kanzariyagovindbhai21@gnu.ac.in>
2022-08-06 10:21:58 +0530

    Checked Given Number is Even or Odd

--- src/main/kotlin/practical4.kt
+++ src/main/kotlin/practical4.kt
@@ -2,10 +2,10 @@ fun main() {
     print("Enter Number : ")
     val x = readLine()!!.toInt()
     println(
-        if (x>=0){
-            "Positive"
+        if (x%2==0){
+            "Even"
         }else{
-            "Negative"
+            "Odd"
         }
     )
 }
