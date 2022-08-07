a9155f558be259430059181ba278ce1d257e762b
Parth <parthbhuva97@gmail.com>
2022-08-07 09:41:42 +0530

    practical3

--- src/main/kotlin/practical3.kt
+++ src/main/kotlin/practical3.kt
@@ -1,5 +1,5 @@
 fun main() {
-    print("Student Enrollment No :")
+    print("Student Enrollment No.:")
     val eno = readLine()!!.toLong()
     print("Student Name:")
     val name = readLine()!!.toLong()
@@ -7,7 +7,26 @@ fun main() {
     val branch = readLine()!!
     print("Student Class:")
     val c = readLine()!!
-    print("Student :")
+    print("Student Batch:")
+    val batch = readLine()!!
+    print("Student College Name:")
+    val college = readLine()!!
+    print("Student University Name:")
+    val uni = readLine()!!
+    print("Student Age:")
+    val age = readLine()!!
+
+    print("************************************")
+
+    print("Student's Data:")
+    print("Enrollment No.:${eno}")
+    print("Name:${name}")
+    print("Age:${age}")
+    print("Branch:${branch}")
+    print("Class:${c}")
+    print("Batch:${batch}")
+    print("College Name::${college}")
+    print("University Name:${uni}")
 
 
 }
